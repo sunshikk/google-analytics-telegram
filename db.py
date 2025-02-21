50f9525a993bdcbd21dcf411811668d1a23e2a33
sunshikk <nvidiartxexe@gmail.com>
2025-02-21 19:10:30 +0500

    Update db.py

--- db.py
+++ db.py
@@ -1,8 +1,6 @@
 import sqlite3
 
-DATABASE_NAME = 'database.db'
-
-conn = sqlite3.connect(f'data/{DATABASE_NAME}')
+conn = sqlite3.connect('/data/database.db')
 cursor = conn.cursor()
 
 cursor.execute("""
